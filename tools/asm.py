1058c750f2a162bd820b4f8e6210c3c1d3c0dd7b
Max Thrun <bear24rw@gmail.com>
2014-04-06 02:46:50 -0400

    keep all bytes in an integer list

--- tools/asm.py
+++ tools/asm.py
@@ -1,11 +1,8 @@
 import sys
 import constants
 
-def bits(number, byte_num=0):
-    number = int(number)
-    number = number >> (8*byte_num)
-    s = bin(number)[2:].zfill(8)[-8:]
-    return s
+def bits(number):
+    return bin(number)[2:].zfill(8)
 
 if __name__ == "__main__":
 
@@ -17,18 +14,12 @@ if __name__ == "__main__":
     rom_file = open(rom_filename, 'wb')
     txt_file = open(txt_filename, 'w')
 
-    num_bytes = 0
+    bytes = []
 
     labels = {}
 
-    def write_byte(x):
-        global num_bytes
-        txt_file.write(x+'\n')
-        rom_file.write(chr(eval('0b'+x)))
-        num_bytes += 1
-
     for _ in range(constants.reset_vector):
-        write_byte(bits(0))
+        bytes.append(0)
 
     for line in asm_file:
 
@@ -39,51 +30,62 @@ if __name__ == "__main__":
             continue
 
         if ":" in line:
-            labels[line.replace(':','')] = num_bytes
+            labels[line.replace(':','')] = len(bytes)
             continue
 
         if line == 'HALT':
-            write_byte(bits(constants.op_codes[line]))
-            write_byte(bits(0))
-            write_byte(bits(0))
-            write_byte(bits(0))
+            bytes.append(constants.op_codes[line])
+            bytes.append(0)
+            bytes.append(0)
+            bytes.append(0)
             continue
 
         op, args = line.split()
 
-        write_byte(bits(constants.op_codes[op]))
+        bytes.append(constants.op_codes[op])
 
         if op in ('JMP'):
-            write_byte(bits(labels[args], 1))
-            write_byte(bits(labels[args], 0))
-            write_byte(bits(0))
+            bytes.append(labels[args] >> 8)
+            bytes.append(labels[args])
+            bytes.append(0)
             continue
 
+        args = [int(x) for x in args.split(",")]
+
         if op in ('LD', 'ST'):
-            d, addr = args.split(',')
-            write_byte(bits(d))
-            write_byte(bits(addr, 1))
-            write_byte(bits(addr, 0))
+            d, addr = args
+            bytes.append(d)
+            bytes.append(addr >> 8)
+            bytes.append(addr)
             continue
 
         if op in ('LDI'):
-            d, addr = args.split(',')
-            write_byte(bits(d))
-            write_byte(bits(addr))
-            write_byte(bits(0))
+            d, value = args
+            bytes.append(d)
+            bytes.append(value)
+            bytes.append(0)
             continue
 
         if op in ('MOV'):
-            d, a = args.split(',')
-            write_byte(bits(d))
-            write_byte(bits(a))
-            write_byte(bits(0))
+            d, a = args
+            bytes.append(d)
+            bytes.append(a)
+            bytes.append(0)
             continue
 
-        d, a, b = args.split(',')
-        write_byte(bits(d))
-        write_byte(bits(a))
-        write_byte(bits(b))
+        d, a, b = args
+        bytes.append(d)
+        bytes.append(a)
+        bytes.append(b)
 
-    for _ in range(constants.ram_size - num_bytes):
-        write_byte(bits(0))
+    for _ in range(constants.ram_size - len(bytes)):
+        bytes.append(0)
+
+    #
+    # Write all bytes out
+    #
+
+    for byte in bytes:
+        txt_file.write(bits(byte)+'\n')
+        rom_file.write(chr(byte))
+
